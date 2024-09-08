6e74847fdadf0c9a58fa60bd0e87e599abf65dfa
vinzentp <43040134+vinzentp@users.noreply.github.com>
2024-09-08 23:00:35 +0200

    edited setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='rage_toolkit',
-    version='0.1.0',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
-        'pandas'
+        'pandas',
     ],
-    author='',
-    author_email='your.email@example.com',
-    description='A framework for retrieval augmented generation evaluation  (RAGE)',
-    long_description=open('README.md').read(),
+    author='Vinzent Penzkofer, Timo Baumann',
+    author_email='vinzent.penzkofer@outlook.de, timo.baumann@oth-regensburg.de',
+    description='A framework for retrieval augmented generation evaluation (RAGE)',
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/',
+    url='https://github.com/othr-nlp/rage_toolkit',
+    license='MIT',
     classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    keywords='retrieval, augmented generation, evaluation, RAG, NLP',
+    include_package_data=True,
 )
